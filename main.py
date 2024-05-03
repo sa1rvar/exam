3c92f2368d67012056ad5a5a4523897e5fabd00f
Sarvar <sabdumutolibov@gmail.com>
2024-05-03 18:23:41 +0500

    exam

---
+++ main.py
@@ -0,0 +1,64 @@
+#task1
+# def toNumberArray(lst):
+#     for m in lst:
+#         return m == int
+#
+# print(toNumberArray(["9.4", "4.2"]))
+# print(toNumberArray(["91", "44"]))
+# print(toNumberArray(["9.5", "8.8"]))
+
+
+#task2
+# def rotateByone(lst):
+#     m = lst.pop(4)
+#     lst.insert(0,m)
+#     return lst
+# print(rotateByone([1,2,3,4,5]))
+# print(rotateByone([6,5,8,9,7]))
+# print(rotateByone([20,15,26,8,4]))
+
+
+#task3
+# def sumMinimus(lst):
+# #     lst1 = []
+# #     for x in lst:
+# #         for n in x:
+# #             if n == x[0]:
+# #                 lst1.append(n)
+# #     return sum(lst1)
+# # print(sumMinimus([
+# #     [1,2,3,4,5],
+# #     [5,6,7,8,9],
+# #     [20,21,34,56,100]
+# # ]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
